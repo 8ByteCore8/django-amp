5c1401db42ffa74529bdfc78b2ce02da6040bc98
Bogdan <kushnir.2001.bogdan@gmail.com>
2020-07-17 16:02:27 +0300

    set project name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="django-amp",
+    name="django-amp_by_ByteCore",
     version="0.0.1",
     author="ByteCore",
     author_email="",
